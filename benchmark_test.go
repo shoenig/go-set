1ea8a300e7b543c2a61422aee1241a98b0abc18d
Seth Hoenig <shoenig@duck.com>
2023-03-21 13:40:40 -0500

    wip more bench

--- benchmark_test.go
+++ benchmark_test.go
@@ -13,28 +13,43 @@ type test struct {
 
 var cases []test = []test{
 	{size: 10, name: "10"},
-	{size: 1_000, name: "1k"},
-	{size: 100_000, name: "100k"},
-	{size: 1_000_000, name: "1m"},
+	{size: 1_000, name: "1000"},
+	{size: 100_000, name: "100000"},
+	{size: 1_000_000, name: "1000000"},
 }
 
-func random(n int) []int {
-	result := make([]int, n)
+func random[I ~int](n int) []I {
+	result := make([]I, n)
 	for i := 0; i < n; i++ {
-		result[i] = rand.Int()
+		result[i] = I(rand.Int())
 	}
 	return result
 }
 
+func unsort[I ~int](s []I) {
+	s[0], s[len(s)-1] = s[len(s)-1], s[0]
+}
+
 type hashint int
 
-func (hi hashint) Hash() int {
-	return int(hi)
+func (i hashint) Hash() int {
+	return int(i)
+}
+
+func BenchmarkSlice_Insert(b *testing.B) {
+	for _, tc := range cases {
+		s := random[int](tc.size)
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				s = append(s, i)
+			}
+		})
+	}
 }
 
 func BenchmarkSet_Insert(b *testing.B) {
 	for _, tc := range cases {
-		s := From(random(tc.size))
+		s := From(random[int](tc.size))
 		b.Run(tc.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				s.Insert(i)
@@ -45,11 +60,7 @@ func BenchmarkSet_Insert(b *testing.B) {
 
 func BenchmarkHashSet_Insert(b *testing.B) {
 	for _, tc := range cases {
-		hs := NewHashSet[hashint, int](tc.size)
-		numbers := random(tc.size)
-		for i := 0; i < tc.size; i++ {
-			hs.Insert(hashint(numbers[i]))
-		}
+		hs := HashSetFrom[hashint, int](random[hashint](tc.size))
 		b.Run(tc.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				hs.Insert(hashint(i))
@@ -60,7 +71,7 @@ func BenchmarkHashSet_Insert(b *testing.B) {
 
 func BenchmarkTreeSet_Insert(b *testing.B) {
 	for _, tc := range cases {
-		ts := TreeSetFrom[int, Compare[int]](random(tc.size), Cmp[int])
+		ts := TreeSetFrom[int, Compare[int]](random[int](tc.size), Cmp[int])
 		b.Run(tc.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				ts.Insert(i)
@@ -69,9 +80,22 @@ func BenchmarkTreeSet_Insert(b *testing.B) {
 	}
 }
 
+func BenchmarkSlice_Minimum(b *testing.B) {
+	for _, tc := range cases {
+		slice := random[int](tc.size)
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				sort.Ints(slice)
+				_ = slice[0]
+				unsort(slice)
+			}
+		})
+	}
+}
+
 func BenchmarkSet_Minimum(b *testing.B) {
 	for _, tc := range cases {
-		s := From(random(tc.size))
+		s := From(random[int](tc.size))
 		b.Run(tc.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				values := s.Slice()
@@ -84,16 +108,13 @@ func BenchmarkSet_Minimum(b *testing.B) {
 
 func BenchmarkHashSet_Minimum(b *testing.B) {
 	for _, tc := range cases {
-		hs := NewHashSet[hashint, int](tc.size)
-		numbers := random(tc.size)
-		for i := 0; i < tc.size; i++ {
-			hs.Insert(hashint(numbers[i]))
-		}
+		hs := HashSetFrom[hashint, int](random[hashint](tc.size))
 		b.Run(tc.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				values := hs.Slice()
 				sort.Slice(values, func(a, b int) bool { return values[a] < values[b] })
 				_ = values[0]
+				unsort(values)
 			}
 		})
 	}
@@ -101,7 +122,7 @@ func BenchmarkHashSet_Minimum(b *testing.B) {
 
 func BenchmarkTreeSet_Minimum(b *testing.B) {
 	for _, tc := range cases {
-		ts := TreeSetFrom[int, Compare[int]](random(tc.size), Cmp[int])
+		ts := TreeSetFrom[int, Compare[int]](random[int](tc.size), Cmp[int])
 		b.Run(tc.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				_ = ts.Min()
@@ -110,13 +131,54 @@ func BenchmarkTreeSet_Minimum(b *testing.B) {
 	}
 }
 
-func BenchmarkSlice_Minimum(b *testing.B) {
+func BenchmarkSlice_Contains(b *testing.B) {
+	contains := func(s []int, target int) bool {
+		for i := 0; i < len(s); i++ {
+			if s[i] == target {
+				return true
+			}
+		}
+		return false
+	}
+
 	for _, tc := range cases {
-		slice := random(tc.size)
+		slice := random[int](tc.size)
 		b.Run(tc.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				sort.Ints(slice)
-				_ = slice[0]
+				_ = contains(slice, i)
+			}
+		})
+	}
+}
+
+func BenchmarkSet_Contains(b *testing.B) {
+	for _, tc := range cases {
+		s := From(random[int](tc.size))
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = s.Contains(i)
+			}
+		})
+	}
+}
+
+func BenchmarkHashSet_Contains(b *testing.B) {
+	for _, tc := range cases {
+		hs := HashSetFrom[hashint, int](random[hashint](tc.size))
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = hs.Contains(hashint(i))
+			}
+		})
+	}
+}
+
+func BenchmarkTreeSet_Contains(b *testing.B) {
+	for _, tc := range cases {
+		ts := TreeSetFrom[int, Compare[int]](random[int](tc.size), Cmp[int])
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_ = ts.Contains(i)
 			}
 		})
 	}
